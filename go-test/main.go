fa1784e004b47fff08e05d1b63c2e6054a724cd6
Zach Holmes <zholmes430@gmail.com>
2024-05-29 19:46:55 +1000

    experiment with go

---
+++ go-test/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println("hello go !!!")
+
+	stationeers_id := 544550
+	steam_json, err := os.Open("./steam_games.json")
+
+	
+
+
+}
